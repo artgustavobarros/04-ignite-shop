032ff0563abe06e3e60203aab36cd5929df713b7
Arthur Barros <135854588+th3gust@users.noreply.github.com>
2024-06-28 12:40:28 -0300

    visual effects

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,42 @@
-import { styled } from "../styles"
-
-const Button = styled('button',{
-  backgroundColor: '$rocketseat',
-  borderRadius: '4px',
-  border: '0px',
-  padding: '4px 8px',
-
-  span:{
-    fontWeight: "bold",
-  },
-
-  "&:hover":{
-    filter: 'brightness(0.8)'
-  }
-
-})
+import Image from "next/image"
+import { HomeContainer, Product } from "../styles/pages/home"
+import tShirt1 from '../assets/t-shirts/1.png'
+import tShirt2 from '../assets/t-shirts/2.png'
+import tShirt3 from '../assets/t-shirts/3.png'
+import tShirt4 from '../assets/t-shirts/4.png'
+import 'keen-slider/keen-slider.min.css'
+import { useKeenSlider } from 'keen-slider/react' 
 
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides:{
+      perView: 3,
+      spacing: 48,
+    }, loop: true,
+  })
   return (
-    <Button>
-      <span>Teste</span>
-      Enviar
-    </Button>
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
+        <Image src={tShirt1} alt='' width={520} height={480}/>
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product className="keen-slider__slide">
+        <Image src={tShirt2} alt='' width={520} height={480}/>
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product className="keen-slider__slide">
+        <Image src={tShirt3} alt='' width={520} height={480}/>
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   )
 }
